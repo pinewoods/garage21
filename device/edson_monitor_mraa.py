4fb2fc6c3c32f9e23ebff7d31cbaf8f9cfa56ae9
Vido <vido@usp.br>
2015-06-20 11:06:09 -0300

    New math

--- device/edson_monitor_mraa.py
+++ device/edson_monitor_mraa.py
@@ -6,8 +6,12 @@ import requests
 import mraa
 import pyupm_i2clcd as lcd
 
-pin = mraa.Aio(0)
+def ratio(_value, from_min=162, from_max=299, to_min=0.0, to_max=100.0):
+    return (float(_value - from_min) * (to_max - to_min) /
+        (from_max - from_min) + to_min)
 
+
+pin = mraa.Aio(0)
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 myLcd.setColor(0, 255, 128)
 while True:
@@ -29,9 +33,9 @@ while True:
 
     myLcd.setCursor(1, 0)
     #distance = 87.09576644*math.exp(-0.004870217643*reading)
-    #tank = 26.0
-    level = (reading-100.0)
-    nivel = 'Nivel: %.2f %%' % (level*100.0)
+    level = ratio(reading)
+    myLcd.write('                ') # clean
+    nivel = 'Nivel: %.2f %%' % (level)
     myLcd.write(nivel)
     print nivel
 
