d0f85134a364af98242fb4033249b84711968e9e
Vido <vido@usp.br>
2015-06-21 21:16:03 -0300

    Testing HCSR04 class

--- device/HC_SR04_mraa.py
+++ device/HC_SR04_mraa.py
@@ -7,7 +7,7 @@ MAX_PERIOD = 7968
 TRIGGER_PULSE = 10
 
 class HCSR04(Object):
-    def __init__(self, triggerPin, echoPin, fptr):
+    def __init__(self, triggerPin, echoPin):
         self.trigger = mraa.Pwm(triggerPin)
         self.echo = mraa.Gpio(echoPin)
         self.echo.dir(mraa.DIR_IN)
@@ -19,7 +19,7 @@ class HCSR04(Object):
         self._pulse_count = 0
         self.echo_response = threading.Event()
 
-    def get_distance (self):
+    def get_distance(self):
         self.trigger.enable(True)
         self.trigger.period_us(MAX_PERIOD)
         self.trigger.pulsewidth_us(TRIGGER_PULSE)
@@ -30,10 +30,17 @@ class HCSR04(Object):
 
         return self.falling_timestamp - self.rising_timestamp
 
-    def ack_edge_detected (self) {
+    def ack_edge_detected(self):
         self._pulse_count += 1
         if _pulse_count % 2 == 0:
             self.falling_timestamp = time.time()
             self.echo_response.set()
         else:
             self.rising_timestamp = time.time()
+
+
+if __name__ == '__main__':
+    my_hcsr04 = HCSR04(7,8)
+    while True:
+        print my_hcsr04.get_distance()
+        time.sleep(1)
