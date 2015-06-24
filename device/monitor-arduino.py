09072b3ab279c105fceda2cd574532959d1cfee6
ismaelrodrigues <rodrigues.ismael@gmail.com>
2015-06-24 20:50:16 -0300

    Python code for ultrasonic sensor

--- device/monitor-arduino.py
+++ device/monitor-arduino.py
@@ -1,13 +1,17 @@
 import time
-import requests
+import urllib
+import urllib2
 
 while True:
-    url = 'http://ws.pinewoods.com.br/api?reading=%d'
     with open('/tmp/arduino.txt','r') as fp:
-    	value = int(fp.read().strip())
-    	print value
+        value = int(fp.read().strip())
+        print value
     try:
-        requests.post(url % value)
+        url = 'http://ws.pinewoods.com.br/api'
+        values = {'reading' : str(value)}
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
     except Exception as e:
         print e
     time.sleep(15)
