da1c730f95c0e0869fae8878e8426979945ef8bf
Vido <vido@usp.br>
2015-07-01 11:13:02 -0300

    Fixing exception of death Jun 30 23:48:11 edison rc.local[283]: Traceback (most recent call last): Jun 30 23:48:11 edison rc.local[283]: File "/home/root/pinewoods/garage21/device/monitor-arduino.py", line 10, in <module> Jun 30 23:48:11 edison rc.local[283]: value = int(fp.read().strip('\0')) Jun 30 23:48:11 edison rc.local[283]: ValueError: invalid literal for int() with base 10: ''

--- device/monitor-arduino.py
+++ device/monitor-arduino.py
@@ -6,12 +6,12 @@ import urllib
 import urllib2
 
 while True:
-    with open('/tmp/arduino.txt','r') as fp:
-        value = int(fp.read().strip('\0'))
-        # print value
     try:
-        #url = 'http://ws.pinewoods.com.br/api'
-        url = 'http://127.0.0.1:5000/api'
+        with open('/tmp/arduino.txt','r') as fp:
+            value = int(fp.read().strip('\0'))
+            # print value
+        url = 'http://ws.pinewoods.com.br/api'
+        #url = 'http://127.0.0.1:5000/api'
         values = {'reading' : str(value)}
         data = urllib.urlencode(values)
         req = urllib2.Request(url, data)
