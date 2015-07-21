1b2d974a10af0ac07bbf1cee36e9c138aae55f8b
Vido <vido@usp.br>
2015-07-20 21:39:00 -0300

    dummy view

--- service2/dashboard/water_meter/views.py
+++ service2/dashboard/water_meter/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 
-# Create your views here.
+from water_meter import WaterTank
+
+class ListWaterTanks(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        us = [user.username for user in User.objects.all()]
+        return Response(usernames)
