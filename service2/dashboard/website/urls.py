8779023c72eb73ef44b3593c00b42d60fa7d9a4a
Vido <vido@usp.br>
2015-08-16 14:54:53 -0300

    Starting settings.html

--- service2/dashboard/website/urls.py
+++ service2/dashboard/website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^dashboard/$', views.index, name='dashboard'),
     url(r'^goals/$', views.goals, name='goals'),
     url(r'^historic/$', views.historic, name='historic'),
+    url(r'^settings/$', views.settings, name='settings'),
 ]
