02a006a0a0d3682b9ba0ee89a586da56a6214279
Vido <vido@usp.br>
2015-08-17 01:05:59 -0300

    Missing forms.py

---
+++ service2/dashboard/sabesp/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from localflavor.br.forms import BRCNPJField
+#from localflavor.br.models import BRCPFField
+from localflavor.br.forms import BRPhoneNumberField
+from localflavor.br.forms import BRZipCodeField
+
+from . import models
+
+class UserProfileForm(forms.ModelForm):
+    cnpj = BRCNPJField(label="CNPJ")
+    phone = BRPhoneNumberField(required=False, label="Telefone")
+    mobile = BRPhoneNumberField(required=False, label="Celular")
+    cep = BRZipCodeField(required=False, label="CEP")
+
+    class Meta:
+        model = models.UserProfile
+        fields = '__all__'
+
+class SabespProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.SabespProfile
+        fields = ['rgi', 'customer_id', 'consumer_type',
+                'supply_unit', 'consumption_goal']
+
+        labels = {
+                'rgi': 'RGI (Registro Geral de Instalação)',
+                'customer_id': 'Código do Cliente',
+                'consumer_type': 'Tipo de Ligação',
+                'supply_unit': 'Reservatório',
+                'consumption_goal': 'Meta Sabesp',
+        }
