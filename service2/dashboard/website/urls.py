cd529869031323742cc172770d21663ba3c930c1
Vido <vido@usp.br>
2015-09-08 21:49:39 -0300

    Redirect to dashboard

--- service2/dashboard/website/urls.py
+++ service2/dashboard/website/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(
+        pattern_name='dashboard', permanent=False)),
     url(r'^dashboard/$', views.index, name='dashboard'),
     url(r'^goals/$', views.goals, name='goals'),
     url(r'^historic/$', views.historic, name='historic'),
