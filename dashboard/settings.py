c20231ce5599d69d4d0fccf683c1bb847e54f39c
afonso <afonso@yack.com.br>
2015-09-09 21:33:49 -0300

    localsettings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -112,9 +112,22 @@ REST_FRAMEWORK = {
 }
 
 NOTIFICATIONS_SOFT_DELETE = True
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
+DEBUG = True
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-try:
-    from .local_settings import *
-except ImportError:
-    print("Fail to import local_settings")
-    pass
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT =path.join(BASE_DIR,'static')
+STATIC_URL =path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
