a75ad97931e4719347705ab357f95bd67a6ede66
afonso <afonso@yack.com.br>
2015-09-11 23:22:20 -0300

    fix

--- dashboard/wsgi.py
+++ dashboard/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
 application = Cling(get_wsgi_application())
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
+
