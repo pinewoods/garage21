bd7cacaa4f546186a0ec306f5fb63d40d0b10c3e
Vido <vido@usp.br>
2015-09-15 03:18:29 -0300

    Final details

--- show_case/static/show_case/js/tempPressureChart.js
+++ show_case/static/show_case/js/tempPressureChart.js
@@ -8,10 +8,10 @@ $(function () {
             text: ''
         },
         xAxis: [{
-            type: 'datetime',
-            dateTimeLabelFormat : {
-                day: '%S'
+            labels:{
+                enabled: false
             },
+            type: 'datetime',
             crosshair: true
         }],
         yAxis: [{ // Primary yAxis
@@ -31,13 +31,13 @@ $(function () {
             title: {
                 text: 'Pressão',
                 style: {
-                    color: Highcharts.getOptions().colors[0]
+                    color: Highcharts.getOptions().colors[1]
                 }
             },
             labels: {
                 format: '{value} bar',
                 style: {
-                    color: Highcharts.getOptions().colors[0]
+                    color: Highcharts.getOptions().colors[1]
                 }
             },
             opposite: true
@@ -46,12 +46,12 @@ $(function () {
             shared: true
         },
         series: [{
-            name: 'Rainfall',
+            name: 'Pressão',
             type: 'spline',
             yAxis: 1,
             data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
             tooltip: {
-                valueSuffix: ' mm'
+                valueSuffix: ' bar'
             }
 
         }, {
@@ -60,7 +60,8 @@ $(function () {
             data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
             tooltip: {
                 valueSuffix: '°C'
-            }
+            },
+            color: Highcharts.getOptions().colors[3]
         }]
     });
 });
